2f7155c9d4e0cc9b3bc4c1d9868925a222d37614
Shwejit Shree <115397123+Shwejit@users.noreply.github.com>
2026-01-18 16:03:51 +0530

    Time: 0 ms (100%), Space: 35.8 MB (29.72%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int sumr=0;
+        //BRUTE FORCE
+        // int sumr=0;
+        // int suml=0;
+        // for(int i=0;i<nums.size();i++){
+        //     int j=i+1;
+        //     int k=i-1;
+        //     sumr=0;
+        //     suml=0;
+        //     while(j<nums.size()){
+        //         sumr+=nums[j];
+        //         j++;
+        //     }
+        //     while(k>=0){
+        //         suml+=nums[k];
+        //         k--;
+        //     }
+        //     if(suml==sumr){
+        //         return i;
+        //     }
+        // }
+        //  return -1;
+
+        //OPTIMAL
+        for(int i=1;i<nums.size();i++){
+            nums[i]+=nums[i-1];
+        }
         int suml=0;
+        int sumr=0;
         for(int i=0;i<nums.size();i++){
-            int j=i+1;
-            int k=i-1;
-            sumr=0;
-            suml=0;
-            while(j<nums.size()){
-                sumr+=nums[j];
-                j++;
+           if(i>0){
+                suml=nums[i-1];
             }
-            while(k>=0){
-                suml+=nums[k];
-                k--;
-            }
+            sumr=nums[nums.size()-1]-nums[i];
             if(suml==sumr){
                 return i;
             }
+            
         }
-         return -1;
+        return -1;
     }
 };
