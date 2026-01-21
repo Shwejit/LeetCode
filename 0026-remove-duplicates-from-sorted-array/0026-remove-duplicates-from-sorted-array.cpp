2866d87d8142dc355603a60a82f4e350ed1ceeb7
Shwejit Shree <115397123+Shwejit@users.noreply.github.com>
2026-01-21 22:31:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        int j=i+1;
+        int count=0;
+        while(j<=nums.size()){
+            if(nums[i]!=nums[j]){
+                nums[i+1]=nums[j];
+                count++;
+                i++;
+            }
+            j++;
+        }
+        return count;
+    }
+};
