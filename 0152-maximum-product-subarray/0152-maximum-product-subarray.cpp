ef2a203de2d5fc65241e421ce269de1e81d2fc75
Shwejit Shree <115397123+Shwejit@users.noreply.github.com>
2026-01-27 19:48:18 +0530

    Time: 361 ms (5.6%), Space: 17.8 MB (23.06%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         //BRUTE FORCE
-        // int product =nums[0];
+        int product =nums[0];
         int maxproduct =nums[0];
         for(int i=0;i<nums.size();i++){
             product =1;
