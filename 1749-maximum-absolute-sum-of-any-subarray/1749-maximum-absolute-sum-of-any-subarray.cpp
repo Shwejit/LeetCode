af205d2775218ecc23d5b8a1d4298f8d4cbbb13f
Shwejit Shree <115397123+Shwejit@users.noreply.github.com>
2026-01-30 00:06:37 +0530

    Time: 0 ms (100%), Space: 45.1 MB (60.07%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
         //OPTIMAL1
-        // int minisum=nums[0];
-        // int maxisum=nums[0];
-        // int maxi = nums[0];
-        // int mini = nums[0];
+        int minisum=nums[0];
+        int maxisum=nums[0];
+        int maxi = nums[0];
+        int mini = nums[0];
 
 
-        // for(int i=1;i<nums.size();i++){
-        //     maxisum=max(nums[i],maxisum+nums[i]);
-        //     maxi=max(maxi,maxisum);
+        for(int i=1;i<nums.size();i++){
+            maxisum=max(nums[i],maxisum+nums[i]);
+            maxi=max(maxi,maxisum);
 
-        //     minisum=min(nums[i],minisum+nums[i]);
-        //     mini=min(mini,minisum);
-        // }
-        // return max(abs(maxi),abs(mini));;
+            minisum=min(nums[i],minisum+nums[i]);
+            mini=min(mini,minisum);
+        }
+        return max(abs(maxi),abs(mini));
 
         //OPTIMAL2
-        int sum=0;
-        int maxsum=0;
-        int minsum=0;
+        // int sum=0;
+        // int maxsum=0;
+        // int minsum=0;
 
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            maxsum=max(maxsum,sum);
-            minsum=min(minsum,sum);
-        }
-        return maxsum-minsum;
+        // for(int i=0;i<nums.size();i++){
+        //     sum+=nums[i];
+        //     maxsum=max(maxsum,sum);
+        //     minsum=min(minsum,sum);
+        // }
+        // return maxsum-minsum;
     }
 };
