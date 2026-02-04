032fd2a8f8b70429b667a70dc669ae70b2788c17
Shwejit Shree <115397123+Shwejit@users.noreply.github.com>
2026-02-04 22:56:12 +0530

    Time: 4 ms (35.5%), Space: 9.7 MB (74.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t){
-            return true;
+        //BRUTE FORCE
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // if(s==t){
+        //     return true;
+        // }
+        // return false;
+        //OPTIMAL
+        unordered_map<char, int> mpp;
+        if(s.length()!=t.length()){
+            return false;
         }
-        return false;
+        for(int i=0;i<s.length();i++){
+            mpp[s[i]]++;
+        }
+        for(int j=0;j<t.length();j++){
+            if(mpp[t[j]]<=0){
+                return false;
+            }
+            mpp[t[j]]--;
+        }
+        return true;
     }
 };
