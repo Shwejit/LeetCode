60f9a4d3973cd411a816d7c64fd80e7b0ea4d1d5
Shwejit Shree <115397123+Shwejit@users.noreply.github.com>
2026-02-06 22:04:29 +0530

    Time: 9 ms (54.2%), Space: 14.6 MB (71.98%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char,int> mpp;
-        for(char ch : s)
-            mpp[ch]++;
-        for(int i = 0; i < s.length(); i++)
-        {
-            if(mpp[s[i]] == 1)
-                return i;
+        //BRUTE FORCE -> using hash map
+        // unordered_map<char,int> mpp;
+        // for(char ch : s)
+        //     mpp[ch]++;
+        // for(int i = 0; i < s.length(); i++)
+        // {
+        //     if(mpp[s[i]] == 1)
+        //         return i;
+        // }
+        // return -1;
+
+        //OPTIMAL -> using an array/vector
+
+        vector<int> countletters(26,0);
+        for(auto &ch : s){
+            countletters[ch-'a']++;
         }
-        
+        for(int i=0;i<s.length();i++){
+            if(countletters[s[i]-'a']==1){
+                return i;
+            }
+        }
         return -1;
     }
 };
