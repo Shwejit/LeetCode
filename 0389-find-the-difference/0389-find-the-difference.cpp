992b52ce629d3abf5e60e463bbde47c7b5abf5de
Shwejit Shree <115397123+Shwejit@users.noreply.github.com>
2026-02-08 23:58:57 +0530

    Time: 0 ms (100%), Space: 9.3 MB (49.97%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char, int> mpp;
+        // unordered_map<char, int> mpp;
+        // for(int i=0;i<s.length();i++){
+        //     mpp[s[i]]++;
+        // }
+        // for(int i=0;i<t.length();i++){
+        //     if(mpp[t[i]]>0){
+        //         mpp[t[i]]--;
+        //     }
+        //     else{
+        //         return t[i];
+        //     }
+        // }
+        // return t[0];
+
+        int xorval=0 ;
         for(int i=0;i<s.length();i++){
-            mpp[s[i]]++;
+            xorval^=s[i];
         }
-        for(int i=0;i<t.length();i++){
-            if(mpp[t[i]]>0){
-                mpp[t[i]]--;
-            }
-            else{
-                return t[i];
-            }
+        for(int j=0;j<t.length();j++){
+            xorval^=t[j];
         }
-        return t[0];
+        return char(xorval);
     }
 };
