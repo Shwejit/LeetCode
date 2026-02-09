e572a69029499093aaf4e32965669c0676c68a56
Shwejit Shree <115397123+Shwejit@users.noreply.github.com>
2026-02-09 23:26:34 +0530

    Time: 8 ms (70.18%), Space: 19 MB (97.83%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -3,27 +3,52 @@ public:
     int countCharacters(vector<string>& words, string chars) {
 
         //BRUTE FORCE
-        unordered_map<char,int> mpp;
-        int count=0;
+        // unordered_map<char,int> mpp;
+        // int count=0;
+        // for(int i=0;i<chars.length();i++){
+        //     mpp[chars[i]]++;
+        // }
+        // for(int j=0;j<words.size();j++){
+        //     string c=words[j];
+        //     count+=c.length();
+        //     unordered_map<char,int> temp = mpp;
+        //     for(int k=0;k<c.length();k++){
+        //         if(temp[c[k]]<=0)
+        //         {
+        //             count-=c.length();
+        //             break;
+        //         }
+        //         else{
+        //             temp[c[k]]--;
+        //         }
+        //     }
+        // }
+        // return count;
+
+        //OPTIMAL
+        int arr1[128]={0};
         for(int i=0;i<chars.length();i++){
-            mpp[chars[i]]++;
+            arr1[chars[i]]++;
         }
+        int len=0;
+        
+
         for(int j=0;j<words.size();j++){
-            string c=words[j];
-            count+=c.length();
-            unordered_map<char,int> temp = mpp;
-            for(int k=0;k<c.length();k++){
-                if(temp[c[k]]<=0)
-                {
-                    count-=c.length();
-                    break;
-                }
-                else{
-                    temp[c[k]]--;
+            int arr2[128]={0};
+            bool ans=true;
+            for(int k=0;k<words[j].length();k++){
+                arr2[words[j][k]]++;
+            }
+            for(int i=0;i<128;i++){
+                if(arr2[i]>arr1[i]){
+                    ans=false;
                 }
             }
+            if(ans==true){
+                len+=words[j].size();
+            }
         }
-        return count;
+        return len;
 
     }
 };
