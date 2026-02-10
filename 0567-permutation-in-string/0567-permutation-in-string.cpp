287a9c41673bb041f79a3eda0ba3d27283945e86
Shwejit Shree <115397123+Shwejit@users.noreply.github.com>
2026-02-10 23:13:17 +0530

    Time: 904 ms (6.33%), Space: 87.5 MB (5%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char, int> mpp;
+        int count = 0;
+        int len = s1.length();
+
+        for (int i = 0; i < s1.length(); i++) {
+            mpp[s1[i]]++;
+        }
+
+        int i = 0;
+        int j = len-1;
+        while (j < s2.length()) {
+            unordered_map<char, int> mpp2=mpp;
+            count=0;
+            for (int k = i; k <= j; k++) {
+                if(mpp2[s2[k]]>0){
+                    mpp2[s2[k]]--;
+                    count++;
+                }
+                
+                if(count==len){
+                    return true;
+                }
+            }
+            i++;
+            j++;
+        }
+        return false;
+    }
+};
