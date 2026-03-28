f56330320cea630b1d44be0bc00ea9c75a588000
Shwejit Shree <115397123+Shwejit@users.noreply.github.com>
2026-03-28 18:51:33 +0530

    Time: 715 ms (7.05%), Space: 23.3 MB (11.61%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,65 +1,29 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        //BRUTE FORCE
-        // unordered_map<char, int> mpp;
-        // int count = 0;
-        // int len = s1.length();
+        unordered_map<char, int> mpp;
+        unordered_map<char, int> mpp1;
 
-        // for (int i = 0; i < s1.length(); i++) {
-        //     mpp[s1[i]]++;
-        // }
-
-        // int i = 0;
-        // int j = len-1;
-        // while (j < s2.length()) {
-        //     unordered_map<char, int> mpp2=mpp;
-        //     count=0;
-        //     for (int k = i; k <= j; k++) {
-        //         if(mpp2[s2[k]]>0){
-        //             mpp2[s2[k]]--;
-        //             count++;
-        //         }
-                
-        //         if(count==len){
-        //             return true;
-        //         }
-        //     }
-        //     i++;
-        //     j++;
-        // }
-        // return false;
-
-        //OPTIMAL
-        unordered_map<char, int> s1count;
-        unordered_map<char, int> s2count;
-
-        if (s1.length() > s2.length()) {
-            return false;
+        for (int i = 0; i < s1.length(); i++) {
+            mpp[s1[i]]++;
         }
-        
-        for(int i=0;i<s1.length();i++){
-            s1count[s1[i]]++;
-            s2count[s2[i]]++;
-        }
-        if(s1count==s2count){
-            return true;
-        }
-        int l=0;
-        int r=s1.length();
-        while(r<s2.length()){
-            s2count[s2[r]]++;
-            s2count[s2[l]]--;
-            
-            if(s2count[s2[l]]==0){
-                s2count.erase(s2[l]);
+        int count=0;
+        int i = 0;
+        int j = s1.length() - 1;
+        while (j < s2.length()) {
+            mpp1 = mpp;
+            for (int k = i; k <= j; k++) {
+                if (mpp1[s2[k]] > 0) {
+                    mpp1[s2[k]]--;
+                    count++;
+                }
             }
-            l++;
-            r++;
-            if(s1count==s2count){
+            if(count==s1.length()){
                 return true;
             }
-            
+            count=0;
+            i++;
+            j++;
         }
         return false;
     }
