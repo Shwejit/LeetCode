908479708a186a49b0138dd44d5208c161c860c6
Shwejit Shree <115397123+Shwejit@users.noreply.github.com>
2026-04-12 23:52:16 +0530

    Time: 1 ms (45.09%), Space: 10.3 MB (23.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-
-        int i=0;
-        int j=s.length()-1;
-        while(i<=j){
-            if(!isalnum(s[i])){
-                i++;
-                continue;
+        string filtered;
+        for (char c : s) {
+            if (isalnum(c)) {
+                filtered += tolower(c);
             }
-            if(!isalnum(s[j])){
-                j--;
-                continue;
-            }
+        }
 
-            if(tolower(s[i])!=tolower(s[j])){
+        int left = 0;
+        int right = filtered.size() - 1;
+
+        while (left < right) {
+            if (filtered[left] != filtered[right]) {
                 return false;
             }
-            i++;
-            j--;
+            left++;
+            right--;
         }
-        return true;
+
+        return true;        
     }
 };
