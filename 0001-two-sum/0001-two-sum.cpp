a7f517d6ac7ef48ce0c6bea6a37f9812a404b2c9
Shwejit Shree <115397123+Shwejit@users.noreply.github.com>
2026-05-07 21:52:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> pairIdx;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            int num = nums[i];
-            if (pairIdx.find(target - num) != pairIdx.end()) {
-                return {i, pairIdx[target - num]};
-            }
-            pairIdx[num] = i;
+    vector<int> twoSum(vector<int>& num, int tar) {
+        unordered_map<int,int> m;
+        for(int i=0;i<num.size();i++){
+            int n=tar-num[i];
+            if(m.count(n)) return {m[n],i};
+            m[num[i]]=i;
         }
-
-        return {};        
+        return {};
     }
+    // It i to DAY ?
 };
