da55dde93304a9613ebdd18fb3cce777f7a741ec
Shwejit Shree <115397123+Shwejit@users.noreply.github.com>
2026-05-14 23:37:44 +0530

    Time: 0 ms (100%), Space: 27.5 MB (14.99%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -3,18 +3,19 @@ public:
     bool isGood(vector<int>& nums) {
         int maxi=INT_MIN;
         unordered_map<int, int> mpp;
-        int ans=0;
+        int count_2=0;
+
         for(int i=0;i<nums.size();i++){
+
             maxi=max(maxi,nums[i]);
             
-            //ans^=nums[i]^i;
-            
             mpp[nums[i]]++;
-            if(mpp[nums[i]]>=2){
-                ans++;
+
+            if(mpp[nums[i]]>=2){   //1,2,3,2,5,5
+                count_2++;
             }
         }
-        if(maxi+1==nums.size() && mpp[maxi]==2 && ans==1){
+        if(maxi+1==nums.size() && mpp[maxi]==2 && count_2==1){
             return true;
         }
         return false;
