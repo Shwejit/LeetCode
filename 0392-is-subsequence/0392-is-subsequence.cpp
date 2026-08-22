5bd9ae1923fc54c9d221a028a930d521e26bf2b6
Jeet <115397123+Shwejit@users.noreply.github.com>
2026-08-23 00:50:00 +0530

    Time: 107 ms (2.06%), Space: 279.6 MB (5.37%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,26 +1,47 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-        int lastFoundIndex=-1;
+    // bool isSubsequence(string s, string t) {
+    //     int lastFoundIndex=-1;
 
-        for(int i=0;i<s.size();i++){
-            bool present=false;
-            for(int j=lastFoundIndex+1;j<t.size();j++){
-                if(s[i]==t[j]){
-                    present=true;
-                    if(j<lastFoundIndex){
-                        return false;
-                    }
-                    lastFoundIndex=j;
-                    break;
-                }
+    //     for(int i=0;i<s.size();i++){
+    //         bool present=false;
+    //         for(int j=lastFoundIndex+1;j<t.size();j++){
+    //             if(s[i]==t[j]){
+    //                 present=true;
+    //                 if(j<lastFoundIndex){
+    //                     return false;
+    //                 }
+    //                 lastFoundIndex=j;
+    //                 break;
+    //             }
                 
-            }
-            if(!present){
-                return false;
-            }
+    //         }
+    //         if(!present){
+    //             return false;
+    //         }
+    //     }
+
+    //     return true;
+    // }
+
+     bool isSubsequence(string s, string t) {
+        return solve(0,0,s,t);
+     }
+
+     bool solve(int i, int j ,string s, string t){
+        if(i==s.size()){
+            return true;
+        }
+        if(j==t.size()){
+            return false;
         }
 
-        return true;
-    }
+        if(s[i]==t[j]){
+            i++;
+        }
+        j++;
+
+
+        return solve(i,j,s,t);
+     }
 };
