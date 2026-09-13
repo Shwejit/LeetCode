e666815cf9427c08e85538589263032b7b01e5ea
Jeet <115397123+Shwejit@users.noreply.github.com>
2026-09-13 22:34:36 +0530

    Time: 76 ms (37%), Space: 111.5 MB (28.31%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        //BRUTE FORCE
-        // for(int i=0;i<nums.size();i++){
-        //     for(int j=i+1;j<nums.size();j++){
-        //         if(nums[i]==nums[j]){
-        //             return true;
-        //         }
-        //     }
-        // }
-        // return false;
-
-        //OPTIMAL SOLUTION
         unordered_map<int,int> mpp;
-        int j=0;
-        while(j<nums.size()){
-            if(mpp.find(nums[j])!=mpp.end()){
+        for(int i=0;i<nums.size();i++){
+            if(mpp.find(nums[i])!=mpp.end()){
                 return true;
             }
-            mpp[nums[j]]=j;
-            j++;
+            mpp[nums[i]]=i;
         }
         return false;
     }
