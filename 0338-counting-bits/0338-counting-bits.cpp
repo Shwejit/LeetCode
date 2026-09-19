6b16102cc22ad9010709122c7476403a01592395
Jeet <115397123+Shwejit@users.noreply.github.com>
2026-09-19 17:48:31 +0530

    Time: 2 ms (35.15%), Space: 11.4 MB (18.43%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,38 +1,17 @@
-// class Solution {
-// public:
-//     vector<int> countBits(int n) {
-//         vector<int> ans;
-//         for(int i=0;i<=n;i++){
-//             ans.push_back(count(i));
-//         }
-//         return ans;
-//     }
-//     int count(int n){
-//         int cnt=0;
-//         if(n<=1){
-//             return n;
-//         }
-//         while(n>0){
-//             int d=n%2;
-//             if(d==1){
-//                 cnt++;
-//             }
-//             n=n/2;
-//         }
-//         return cnt;
-//     }
-// };
-
-// OPTIMAL:
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> dp(n + 1, 0);
-
-        for (int i = 1; i <= n; i++) {
-            dp[i] = dp[i / 2] + (i % 2);
+        vector<int> ans;
+        ans.push_back(0);
+        for(int i=1;i<=n;i++){
+            int  count=0;
+            int v=i;
+            while(v>0){
+                v=v&(v-1);
+                count++;
+            }
+            ans.push_back(count);
         }
-
-        return dp;
+        return ans;
     }
 };
